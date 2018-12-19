7edb269a08d7421309d6a113be4484a34ce52c65
Aditya Respati <dityo.hario@gmail.com>
2018-12-19 16:01:01 +0700

    lagi-bikin-home

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ const app = express ()
 const user = require('./routes/routes-user')
 const book = require('./routes/routes-books')
 const genre = require('./routes/routes-genres')
+const home = require('./routes/routes-home')
 
 app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
+app.use('/home', home)
 app.use('/user', user)
 app.use('/book', book)
 app.use('/genre', genre)
+
 
 app.listen(3000, ()=>{
   console.log('server is listening on port 3000....');
