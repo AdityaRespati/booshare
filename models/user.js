5996bedb0f20deefcb5cb16cb8a8511600e3eafe
Theresia Coanata <theresiacoa@DESKTOP-K5BVEFS.localdomain>
2018-12-20 17:05:42 +0700

    session done

--- models/user.js
+++ models/user.js
@@ -1,13 +1,38 @@
 'use strict';
 const encrypt = require('../helpers/encrypt');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 module.exports = (sequelize, DataTypes) => {
   const user = sequelize.define('user', {
     username: DataTypes.STRING,
-    email: DataTypes.STRING,
+
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isUnique: function (value, next) {
+          user.findOne({
+            where: {email: value,
+            id: {[Op.ne]: this.id}
+          }
+          })
+          .then(data => {
+            if(data) {
+              next(`email already in use`)
+            } else {
+              next()
+            }
+          })
+          .catch(err => {
+            next(err)
+          })
+        }
+      }
+    },
     password: DataTypes.STRING,
     secret: DataTypes.STRING
-  }, {
+  }, 
+  {
     hooks: {
       beforeCreate: (value) => {
         let generate = encrypt(value.password);
@@ -19,5 +44,7 @@ module.exports = (sequelize, DataTypes) => {
   user.associate = function(models) {
     // associations can be defined here
   };
+
+
   return user;
 };
